adc9537d5c9481fcea63204ba5db06d1d0f2cacf
Everett Gally <egally1@gulls.salisbury.edu>
2021-03-29 09:30:00 -0400

    adding cpp and header files for aes class adding makefile

---
+++ AesObj.cpp
@@ -0,0 +1,5 @@
+//
+// Created by guy on 3/29/21.
+//
+
+#include "AesObj.h"
---
+++ AesObj.h
@@ -0,0 +1,14 @@
+//
+// Created by guy on 3/29/21.
+//
+
+#ifndef COSC370_PROJECT_AESOBJ_H
+#define COSC370_PROJECT_AESOBJ_H
+
+
+class AesObj {
+
+};
+
+
+#endif //COSC370_PROJECT_AESOBJ_H
