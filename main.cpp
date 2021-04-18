96eddf481dfa7216538fc247782c6d7736dce6bb
Everett Gally <egally1@gulls.salisbury.edu>
2021-04-18 13:01:38 -0400

    testing out faster galoisMultiply function

--- main.cpp
+++ main.cpp
@@ -40,20 +40,20 @@ int main()
 //    aes256KeyExpand.encrypt(aes128plaintext, 16, aes256keyExpansionCipherKey);
 
 
-    vector<unsigned char> encryptedData;
-
-    IV iv(2);
-
-    AesEncryptObj aes128(128);
-    encryptedData = aes128.encrypt(aes128plaintext, 16, aes128key, iv);
-
-
-
-    for(const unsigned char & temp : encryptedData)
-    {
-        cout << std::hex << (int) temp;
-    }
-    cout << endl << endl;
+//    vector<unsigned char> encryptedData;
+//
+//    IV iv(2);
+//
+//    AesEncryptObj aes128(128);
+//    encryptedData = aes128.encrypt(aes128plaintext, 16, aes128key, iv);
+//
+//
+//
+//    for(const unsigned char & temp : encryptedData)
+//    {
+//        cout << std::hex << (int) temp;
+//    }
+//    cout << endl << endl;
 
 
 
@@ -117,7 +117,14 @@ int main()
 
 
 
+    byte left = 0x57;
+    byte right = 0x83;
 
+
+    left.galoisMultiply(right);
+
+
+    cout << std::hex << (int) left.rawData() << endl;
 
 
 
